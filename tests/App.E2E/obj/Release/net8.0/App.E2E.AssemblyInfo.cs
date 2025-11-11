efaa8afd4b4cc3bfadba248eef594c3d8d057e78
Rovo Dev <dev@rovodev.local>
2025-11-11 15:02:45 +1100

    chore: package portable zip and attempt MSIX

--- tests/App.E2E/obj/Release/net8.0/App.E2E.AssemblyInfo.cs
+++ tests/App.E2E/obj/Release/net8.0/App.E2E.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("App.E2E")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90f2da9ef7c079e2dfb340c406a7c76b267ee047")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a63803a810b54813fd68a5afba227f811b9d0f8a")]
 [assembly: System.Reflection.AssemblyProductAttribute("App.E2E")]
 [assembly: System.Reflection.AssemblyTitleAttribute("App.E2E")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
