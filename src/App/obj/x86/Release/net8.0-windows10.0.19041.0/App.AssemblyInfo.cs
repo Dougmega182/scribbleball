90f2da9ef7c079e2dfb340c406a7c76b267ee047
Rovo Dev <dev@rovodev.local>
2025-11-11 15:01:58 +1100

    test: fix geometry test structure; build+tests pass

--- src/App/obj/x86/Release/net8.0-windows10.0.19041.0/App.AssemblyInfo.cs
+++ src/App/obj/x86/Release/net8.0-windows10.0.19041.0/App.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0138bc27b453aa6368e91225e16cf22b9f19d43e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2077de2953559b38090680fbfe0226dbf16307d5")]
 [assembly: System.Reflection.AssemblyProductAttribute("App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
